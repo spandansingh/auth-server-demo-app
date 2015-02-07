c1931f7a79d67431c5d302ff17f6a7cd332752d0
spandansingh <developer.spandan@gmail.com>
2015-02-07 16:45:07 +0530

    object club to one file

--- app/init.php
+++ app/init.php
@@ -2,3 +2,11 @@
 <?php
 
 require dirname(__DIR__).'/vendor/autoload.php';
+
+use Auth\ApiClient;
+
+$auth = new ApiClient(array(
+	'KEY' =>  '4783924789374897238947923',  // YOUR AUTH KEY 
+	'SECRET' =>  '$2y$10$qT8gRka/U4A.2hm5cXVtkuBcKLCV0Impo72DNOQl6Rz55Z2rYWwTa',  // YOUR AUTH SECRET
+	'LOGIN_URI' =>   '/login.php', // YOUR APP LOGIN URL
+));
