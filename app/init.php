078a74b73a75d981b5d507cb6e0e0cedbc99b7a6
spandansingh <developer.spandan@gmail.com>
2015-03-04 11:08:24 +0530

    Auth Changes to AuthServer

--- app/init.php
+++ app/init.php
@@ -1,12 +1,12 @@
 
 <?php
-
+session_start();
 require dirname(__DIR__).'/vendor/autoload.php';
 
-use Auth\ApiClient;
+use AuthServer\ApiClient;
 
 $auth = new ApiClient(array(
-	'KEY' =>  '4783924789374897238947923',  // YOUR AUTH KEY 
-	'SECRET' =>  '$2y$10$qT8gRka/U4A.2hm5cXVtkuBcKLCV0Impo72DNOQl6Rz55Z2rYWwTa',  // YOUR AUTH SECRET
+	'KEY' =>  '1015466341',  // YOUR AUTH KEY 
+	'SECRET' =>  '8OxDzE5minDU93h27G0R2pmeml1fhiWX44KZ0j6D',  // YOUR AUTH SECRET
 	'LOGIN_URI' =>   '/login.php', // YOUR APP LOGIN URL
 ));
